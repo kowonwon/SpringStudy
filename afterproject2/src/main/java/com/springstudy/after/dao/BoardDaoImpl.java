39e6fa357bfef1f279b3af2d144708cfd3785a08
kowon <wong11411@gmail.com>
2024-06-09 14:48:37 +0900

    add

--- afterproject2/src/main/java/com/springstudy/after/dao/BoardDaoImpl.java
+++ afterproject2/src/main/java/com/springstudy/after/dao/BoardDaoImpl.java
@@ -2,13 +2,20 @@ package com.springstudy.after.dao;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.springstudy.after.domain.Board;
 
+@Repository
 public class BoardDaoImpl implements BoardDao{
 
+	private final String NAME_SPACE = "com.springstudy.after.mapper.BoardMapper";
+	private SqlSessionTemplate sqlSession;
+	
 	@Override
 	public List<Board> boardList() {
-		return null;
+		return sqlSession.selectList(NAME_SPACE + ".boardList");
 	}
 
 	@Override
